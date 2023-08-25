0bb11f87336926b50a98e9a23fea9edf2668fe3b
acl26 <142262109+acl26@users.noreply.github.com>
2023-08-25 14:59:26 -0400

    HW 19
    Code written, test not passing

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,72 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import java.time.Duration;
+
 
 public class BaseTest {
 
+    public WebDriver driver = null;
+   public String url;
+//    public String url = "https://qa.koel.app";
+
     @BeforeSuite
-    static void setupClass() {
+    static void setupClass() { WebDriverManager.chromedriver().setup(); }
+
+    @BeforeMethod
+    @Parameters({"BaseURL"})
+    public void launchBrowser(String baseURL) {
         WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        url=baseURL;
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+
+    }
+
+    public void navigateToPage() {
+        driver.get(url);
+    }
+
+    public void provideEmail() {
+        WebElement loginInput = driver.findElement(By.cssSelector("[type='email']"));
+        loginInput.click();
+        loginInput.clear();
+        loginInput.sendKeys("anna.gertzen@testpro.io");
+    }
+
+    public void providePassword() {
+        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
+        passwordInput.click();
+        passwordInput.clear();
+        passwordInput.sendKeys("te$t$tudent");
+    }
+
+    public void clickSubmit() {
+        WebElement loginBtn = driver.findElement(By.cssSelector("[type='submit']"));
+        loginBtn.click();
+}
+
+    protected void clickPlaylist() {
+     WebElement selectedPlaylist = driver.findElement(By.cssSelector("#playlists> ul> li:nth-child(4)"));
+     selectedPlaylist.click();
+    }
+
+    protected void clickRedBtn() {
+     WebElement redBtn = driver.findElement(By.cssSelector("title='Delete this playlist'"));
+     clickRedBtn();
     }
 }
+
+
+
+
---
+++ src/test/java/Homework19.java
@@ -0,0 +1,46 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class Homework19 extends BaseTest {
+ String notification = "\"Deleted playlist TestPRo\"";
+ @Test
+ public void deletePlaylist() throws  InterruptedException {
+     WebElement getDeletePlaylistMessage = driver.findElement(By.cssSelector(".alertify-logs.top.right"));
+
+        navigateToPage();
+        provideEmail();
+        providePassword();
+        clickSubmit();
+        Thread.sleep(2000);
+        clickPlaylist();
+        Thread.sleep(2000);
+        clickRedBtn();
+        Thread.sleep(2000);
+        Assert.assertEquals(getDeletePlaylistMessage, notification);
+
+    }
+}
+
+//        1. Create a new branch named homework-19.
+//
+//        2. Create a new file Homework19.java in IntelliJ IDEA.
+//
+//        3. Create a test case, deletePlaylist() using @Test annotation.
+//
+//        4. Use the helper/reusable methods we created.
+//
+//        5. Use @Parameters for passing baseUrl from the TestNG config file to the tests.
+//
+//        6. Navigate to "https://qa.koel.app/".
+//
+//        7. Log in with your credentials.
+//
+//        8. Click the playlist you want to delete.
+//
+//        9. We should see the red button "x PLAYLIST" on the top right part of the page, and click on it.
+//
+//        Note: If the red button is not displayed, let's create a new playlist and click on it.
+//
+//        10. Verify that the confirmation notification displayed has the text, "Deleted playlist {playlist name}".
